2634068671c1cc55efb9038009aba81ae5bfdc3b
Gavin Barnett <gavbarnett@gmail.com>
2017-11-04 22:38:09 +0000

    Basic Maths
    Doesn't support brackets yet.
    Supports Engineering notation and E notation.

--- js/terminal.js
+++ js/terminal.js
@@ -1,6 +1,7 @@
 document.onkeydown = checkKey;
 document.onclick = checkMouse;
 var memory = [];
+var answer = 0;
 var user = "gavbarnett.github.io";
 var dir = "/"
 
@@ -11,12 +12,117 @@ function load(){
 }
 function OS(input){
   //if input is a keyword
-  var output = keyword(input)
+  var output = keyword(input);
   if (output != ""){
     return output;
   } else {
-    return "Not a Keyword"
+    //return "Not a Keyword";
+    output = toengnot(calculator(input));
+    answer = output;
+    return output;
+  }
+  return ("I don't understand!");
 
+  function fromsymbol(input){
+    var output = input;
+    output = output.replace("pi",Math.PI);
+    output = output.replace('"', answer);
+    output = fromengnot(output);
+    return (output);
+  }
+
+  function fromengnot(input){
+    var output = input;
+    output = output.replace("Y","e24");
+    output = output.replace("Z","e21");
+    output = output.replace("E","e18");
+    output = output.replace("P","e15");
+    output = output.replace("T","e12");
+    output = output.replace("G","e9");
+    output = output.replace("M","e6");
+    output = output.replace("k","e3");
+    output = output.replace("m","e-3");
+    output = output.replace("u","e-6");
+    output = output.replace("n","e-9");
+    output = output.replace("p","e-12");
+    output = output.replace("f","e-15");
+    output = output.replace("a","e-18");
+    output = output.replace("z","e-21");
+    output = output.replace("y","e-24");
+    return (output);
+  }
+  function toengnot(input){
+    var output = Number(input).toFixed(2);
+    if (input >= 1e24) {return (input/1e24).toFixed(2) + "Y";}
+    if (input >= 1e21) {return (input/1e21).toFixed(2) + "Z";}
+    if (input >= 1e18) {return (input/1e18).toFixed(2) + "E";}
+    if (input >= 1e15) {return (input/1e15).toFixed(2) + "P";}
+    if (input >= 1e12) {return (input/1e12).toFixed(2) + "T";}
+    if (input >= 1e9) {return (input/1e9).toFixed(2) + "G";}
+    if (input >= 1e6) {return (input/1e6).toFixed(2) + "M";}
+    if (input >= 1e3) {return (input/1e3).toFixed(2) + "k";}
+    if (input < 1e-21) {return (input/1e-24).toFixed(2) + "y";}
+    if (input < 1e-18) {return (input/1e-21).toFixed(2) + "z";}
+    if (input < 1e-15) {return (input/1e-18).toFixed(2) + "a";}
+    if (input < 1e-12) {return (input/1e-15).toFixed(2) + "f";}
+    if (input < 1e-9) {return (input/1e-12).toFixed(2) + "p";}
+    if (input < 1e-6) {return (input/1e-9).toFixed(2) + "n";}
+    if (input < 1e-3) {return (input/1e-6).toFixed(2) + "u";}
+    if (input < 1e0) {return (input/1e-3).toFixed(2) + "m";}
+    return (output);
+  }
+  function calculator(input){
+    //supports E/e notation (10E3 = 10e3 = 10,000)
+    //supports Engineeing notion (10k = 10,000)
+    //supports common number base (0b10 = binary 10 = decimal 2)
+    var equation = [];
+    var i = 0;
+    if (!isNaN(input)) {
+      output = input;
+      return Number(output);
+    }
+    //brackets
+    if (input.includes("+")){
+      equation = input.split("+");
+      output = calculator(equation[0]);
+      for (i=1; i<equation.length; i++){
+        output += calculator(equation[i]);
+      }
+      return (output);
+    }
+    if (input.includes("-")){
+      equation = input.split("-");
+      output = calculator(equation[0]);
+      for (i=1; i<equation.length; i++){
+        output -= calculator(equation[i]);
+      }
+      return (output);
+    }
+    if (input.includes("*")){
+      equation = input.split("*");
+      output = calculator(equation[0]);
+      for (i=1; i<equation.length; i++){
+        output *= calculator(equation[i]);
+      }
+      return (output);
+    }
+    if (input.includes("/")){
+      equation = input.split("/");
+      output = calculator(equation[0]);
+      for (i=1; i<equation.length; i++){
+        output /= calculator(equation[i]);
+      }
+      return (output);
+    }
+    if (input.includes("^")){
+      equation = input.split("^");
+      output = calculator(equation[0]);
+      for (i=1; i<equation.length; i++){
+        output = Math.pow(output,calculator(equation[i]));
+      }
+      return (output);
+    }
+    return fromsymbol(input);
   }
 
   function keyword(input){
@@ -40,7 +146,6 @@ function OS(input){
     }
     return output;
   }
-
 }
 
 function checkKey(e) {
