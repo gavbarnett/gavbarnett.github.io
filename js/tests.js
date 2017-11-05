be561fbf18ac2bed2533b5bca0286a586c137a92
Gavin Barnett <gavbarnett@gmail.com>
2017-11-05 18:16:17 +0000

    Added Tests
    Added some basic tests. Need to expand

--- js/tests.js
+++ js/tests.js
@@ -1,3 +1,23 @@
 function tests(){
-  console.log ("Tests Passed")
+  var testresults = [];
+  var test_flag = 0;
+  function OS_tester(testname, test, result){
+        if (OS(test) != result){
+          console.log(testname + ": FAILED");
+          console.log(testname + ": " + OS(test) + " != " + result );
+          testresults[testresults.length] = [testname, "FAILED"];
+          test_flag = 1;
+        } else {
+          testresults[testresults.length] = [testname, "PASSED"];
+        }
+  }
+  OS_tester("1=1","1","= 1.00");
+  OS_tester("1+1","1+1","= 2.00");
+  OS_tester("2^2","2^2","= 4.00");
+  OS_tester("2^2","2^2","= 4.00");
+
+  if (test_flag ==0 ){
+    console.log ("All tests PASSED");
+    console.log (testresults);
+  }
 }
